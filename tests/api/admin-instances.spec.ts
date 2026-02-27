37aba8449a849d547f195c6e2398b044d503d141
Haidenok <mykola@launchnyc.io>
2026-02-27 13:01:21 +0100

    Initial commit

--- tests/api/admin-instances.spec.ts
+++ tests/api/admin-instances.spec.ts
@@ -3,13 +3,13 @@ import { AdminInstancesRepository } from '../../src/api/repositories/AdminInstan
 import { AdminInstancesValidator } from '../../src/api/validators/AdminInstancesValidator';
 
 test('GET /admin/instances returns valid instances list', async ({ request }) => {
-  const repository = new AdminInstancesRepository(
-    request,
-    process.env.API_BASE_URL ?? 'https://localhost:5199'
-  );
+    const repository = new AdminInstancesRepository(
+        request,
+        process.env.API_BASE_URL ?? 'https://localhost:5199'
+    );
 
-  const response = await repository.getAdminInstancesRaw();
+    const response = await repository.getAdminInstancesRaw();
 
-  const validator = new AdminInstancesValidator();
-  await validator.validate(response);
+    const validator = new AdminInstancesValidator();
+    await validator.validate(response);
 });
