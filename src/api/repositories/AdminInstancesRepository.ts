6a1afdc96e486e32ab2cdce331de07bd00cd11ee
mykolaLaunch <mykola@launchnyc.io>
2026-02-27 11:34:30 +0100

    Refactor API repositories to base style with endpoint/url processing

--- src/api/repositories/AdminInstancesRepository.ts
+++ src/api/repositories/AdminInstancesRepository.ts
@@ -1,14 +1,22 @@
 import { APIRequestContext, APIResponse } from '@playwright/test';
+import { AdminInstance } from '../models/adminInstances';
 import { BaseApiRepository } from './BaseApiRepository';
 
 export class AdminInstancesRepository extends BaseApiRepository {
+  protected endpoint = '/admin/instances';
+
   constructor(request: APIRequestContext, apiBaseUrl: string) {
     super(request, apiBaseUrl);
   }
 
-  public async getAdminInstances(): Promise<APIResponse> {
-    return this.get('/admin/instances', {
-      accept: 'text/plain'
+  public async getAdminInstancesRaw(): Promise<APIResponse> {
+    return this.request.get(this.getCompleteUrl(), {
+      headers: this.getResponseTypeHeaders(false)
     });
+  }
+
+  public async getAdminInstances(): Promise<AdminInstance[]> {
+    const response = await this.getAdminInstancesRaw();
+    return this.processSuccessResponse<AdminInstance[]>(response);
   }
 }
